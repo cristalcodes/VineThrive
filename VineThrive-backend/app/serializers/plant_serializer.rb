6a379a6ec45346da178ca8a03d9bff3857d64323
Huda Yousif <hjoski@gmail.com>
2020-02-10 21:48:32 -0800

    update serializer

--- VineThrive-backend/app/serializers/plant_serializer.rb
+++ VineThrive-backend/app/serializers/plant_serializer.rb
@@ -1,3 +1,4 @@
 class PlantSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :description, :light, :water, :price
+  belongs_to :stages 
 end
---
+++ VineThrive-backend/app/serializers/stage_serializer.rb
@@ -0,0 +1,4 @@
+class StageSerializer < ActiveModel::Serializer
+  attributes :id, :seed, :small, :large
+  belong_to :plant 
+end
