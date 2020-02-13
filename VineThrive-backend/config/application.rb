f8c9be208d2031f5f38db432ed0c19d36028865e
Huda Yousif <hjoski@gmail.com>
2020-02-12 21:04:50 -0800

    create method in application

--- VineThrive-backend/config/application.rb
+++ VineThrive-backend/config/application.rb
@@ -21,6 +21,14 @@ Bundler.require(*Rails.groups)
 
 module VineThriveBackend
   class Application < Rails::Application
+
+    config.middlewear.insert_before 0, Rack::Cors do 
+      allow do 
+        origins '*'
+        resource '*', headers: :any, methods[:get, :post]
+      end 
+    end 
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
