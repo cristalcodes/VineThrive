9dff752e74020880b84d8d6581f9c8dee745e0fc
Huda Yousif <hjoski@gmail.com>
2020-02-15 11:43:02 -0800

    update resources in application

--- VineThrive-backend/config/application.rb
+++ VineThrive-backend/config/application.rb
@@ -25,7 +25,7 @@ module VineThriveBackend
     config.middleware.insert_before 0, Rack::Cors do 
       allow do 
         origins '*'
-        resource '*', headers: :any, methods:[:get, :post]
+        resource '*', headers: :any, methods:[:get, :post, :delete, :patch, :options]
       end 
     end 
     
