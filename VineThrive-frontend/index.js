2cc4aab35c1baa7f308e3c0903d08dcab89824b6
Huda Yousif <hjoski@gmail.com>
2020-02-15 11:43:20 -0800

    update buttons

--- VineThrive-frontend/index.js
+++ VineThrive-frontend/index.js
@@ -12,9 +12,10 @@ function getPlants(){
     fetch(BASE_URL+'/plants')
     .then(resp => resp.json())
     .then(plants => {
+        console.log(plants);
         main.innerHTML+= plants.map(plant =>  `
-        <li><a href="#" data-id="${plant.id}">${plant.description}</a> 
-        - ${plant.price} - ${plant.light} - ${plant.water}
+        <li><a href="#" data-id="${plant.id}">${plant.name}</a>
+        ${plant.description} - ${plant.price} - ${plant.light} - ${plant.water}
         <button data-id=${plant.id} onclick="removePlant(${plant.id})"; return false;>Delete</button>
         <button data-id=${plant.id} onclick="editPlant(${plant.id})"; return false;>Edit</button>
         </li>
@@ -65,8 +66,10 @@ function displayCreateForm() {
         <input type="submit" value="Create Plant"><br>
 
     `
-    plantFormDiv.innerHTML = html
+    document.getElementById("plant-form").innerHTML = html
 }
+
+//add new  plant to database
 
 function createPlant() {
     const plant = {
@@ -87,8 +90,8 @@ function createPlant() {
     .then(resp => resp.json())
     .then(plant => {
         document.querySelector('#main').innerHTML += `
-        <li><a href="#" data-id="${plant.id}">${plant.description}</a>
-        - ${plant.price} - ${plant.light} - ${plant.water}
+        <li><a href="#" data-id="${plant.id}">${plant.name}</a>
+        ${plant.description} - ${plant.price} - ${plant.light} - ${plant.water}
          <button data-id=${plant.id} onclick="removePlant(${plant.id})"; return false;>Delete</button>
          <button data-id=${plant.id} onclick="editPlant(${plant.id})"; return false;>Edit</button>
          </li>
@@ -119,9 +122,72 @@ function removePlant(id){
     fetch(BASE_URL + `/plants/${id}`, {
         method: "DELETE",
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json' ,
             'Accept': 'application/json'
         }
     })
     .then(event.target.parentElement.remove())
+}
+
+function editPlant(id){
+    clearForm()
+    fetch(BASE_URL + `/plants/${id}`)
+    .then(resp => resp.json())
+    .then(plant => {
+        let plantForm = document.getElementById("plant-form") 
+        let html = `
+        <form onsubmit="updatePlant(${id});return false;">
+        <label>Name:</label>
+        <input type="text" id="name" value="${plant.name}"><br>
+
+        <label>Description:</label>
+        <textarea type="text" id="description" value="${plant.description}"></textarea><br>
+
+        <label>Price:</label>
+        <input type="text" id="price" value="${plant.price}"><br>
+
+        <label>Light:</label>
+        <input type="text" id="light" value="${plant.light}"><br>
+
+        <label>Water</label>
+        <input type="text" id="water" value="${plant.water}"><br>
+
+        <input type="submit" value="Update Plant"><br>
+        </form>
+        ` 
+       
+    plantForm.innerHTML = html
+    })
+}
+
+function updatePlant(id){
+    const plant = {
+        "name": document.getElementById('name').value,
+        "description": document.getElementById('description').value,
+        "price": document.getElementById('price').value,
+        "light": document.getElementById('light').value,
+        "water": document.getElementById('water').value 
+    }
+    fetch(BASE_URL + `/plants/${id}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(plant)
+    })
+    .then(resp => resp.json())
+    .then((plant)=> {
+            document.querySelectorAll(`li a[data-id="${id}"]`)[0].parentElement.innerHTML =  `
+            <li><a href="#" data-id="${plant.id}">${plant.name}</a>
+            ${plant.description} - ${plant.price} - ${plant.light} - ${plant.water}
+             <button data-id=${plant.id} onclick="removePlant(${plant.id})"; return false;>Delete</button>
+             <button data-id=${plant.id} onclick="editPlant(${plant.id})"; return false;>Edit</button>
+             </li>
+            `
+            attachClickPlantLinks()
+            clearForm()
+        }
+    
+    )
 }
